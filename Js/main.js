6f541e02c77a956964659fefbd2a00166ff60053
Anamika-kailey <anamikakailey04@gmail.com>
2025-03-07 20:11:43 +0530

    create landing page

---
+++ Js/main.js
@@ -0,0 +1,7 @@
+document.querySelector(".search-icon").addEventListener("click", function() {
+    document.querySelector(".search-bar").classList.toggle("active");
+});
+
+document.querySelector(".menu-icon").addEventListener("click", function() {
+    document.querySelector(".menu").classList.toggle("active");
+});
