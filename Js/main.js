eae763475945f84a23a4ba10aad859c81451c9a0
Anamika-kailey <anamikakailey04@gmail.com>
2025-03-12 23:03:52 +0530

    fix issues

--- Js/main.js
+++ Js/main.js
@@ -2,6 +2,14 @@ document.querySelector(".search-icon").addEventListener("click", function() {
     document.querySelector(".search-bar").classList.toggle("active");
 });
 
-document.querySelector(".menu-icon").addEventListener("click", function() {
+document.querySelector(".menu-icon-dynamic").addEventListener("click", function() {
     document.querySelector(".menu").classList.toggle("active");
 });
+
+const menuIcon = document.querySelector(".menu-icon-dynamic");
+const menu = document.querySelector(".menu");
+document.addEventListener("click", function (event) {
+    if (!menu.contains(event.target) && !menuIcon.contains(event.target)) {
+        menu.classList.remove("active");
+    }
+})
